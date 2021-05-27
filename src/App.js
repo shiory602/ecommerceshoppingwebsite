8aaab1cd9c0ab31aced108cd5662bdc6f8a3558a
shiory602 <suzushioufo@gmail.com>
2021-05-27 16:59:30 -0700

    set images

--- src/App.js
+++ src/App.js
@@ -1,32 +1,12 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import Top from './components/Top/Top.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
     <div className="App">
-      <div className="container">
-        <div className="row">
-          <Header />
-        </div>
-        <div className="row">
-          <h2>banner</h2>
-        </div>
-        <div className="row">
-          <h2>main</h2>
-          <div className="col">col</div>
-          <div className="col">col</div>
-          <div className="col">col</div>
-        </div>
-        <div className="row">
-          <h1>feature</h1>
-        </div>
-        <div className="row">
-          <Footer />
-        </div>
-      </div>
+          <Top />
     </div>
   );
 }
--- src/components/Top/Top.jsx
+++ src/components/Top/Top.jsx
@@ -1,9 +1,19 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import "./Top.css";
 import Header from '../Header';
 import Footer from '../Footer';
+import banner from "./banner.jpg";
+import dress from "./dress.jpg";
+import shoes from "./shoes.jpg";
+import bag from "./bag.jpg";
 
 export default function Top() {
+    // const [products, setProducts] = useState([]);
+    // useEffect(() => {
+    //     fetch('https://fakestoreapi.com/products')
+    //         .then((res) => res.json())
+    //         .then((data) => setProducts(data));
+    // }, []);
     return(
     <>
     <div className="container">
@@ -11,16 +21,23 @@ export default function Top() {
             <Header />
         </div>
         <div className="row">
-            <h2>banner</h2>
+            <img src={banner} alt="banner" width="100%" />
         </div>
         <div className="row">
-            <h2>main</h2>
-            <div className="col">col</div>
-            <div className="col">col</div>
-            <div className="col">col</div>
+            <h2>Categories</h2>
+            <div className="col">
+                <img src={dress} alt="dress" width="100%" />
+            </div>
+            <div className="col">
+                <img src={shoes} alt="shoes" width="100%" />
+            </div>
+            <div className="col">
+                <img src={bag} alt="bag" width="100%" />
+            </div>
         </div>
         <div className="row">
-            <h1>feature</h1>
+            <h2>feature</h2>
+            <img src={banner} alt="banner" width="100%" />
         </div>
         <div className="row">
             <Footer />
