47985f4b1773313c51b9e6864b44013f0c733156
unknown <gerardomontme@gmail.com>
2021-05-28 13:31:49 -0700

    Route

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,17 @@
 import React from "react";
 import logo from "../logo/logo.png";
+import { Link } from "react-router-dom";
+
 
 export default function Header () {
     return (
         <>
         <ul className="nav d-flex justify-content-between">
             <li className="nav-item row">
-                <a className="nav-link active col" aria-current="page" href="#">
-                    <span classNameName="mx-3">Dress</span>
-                </a>
-                <a className="nav-link active col" aria-current="page" href="#">
-                    <span classNameName="mx-3">Shoes</span>
-                </a>
-                <a className="nav-link active col" aria-current="page" href="#">
-                <span classNameName="mx-3">Accessories</span>
-                </a>
+                <Link to="/">Home</Link>
+            </li>
+            <li className="nav-item">
+                <Link to="/product">Product</Link>
             </li>
             <li className="nav-item">
                 <a className="nav-link" href="#">
@@ -23,10 +20,10 @@ export default function Header () {
             </li>
             <li className="nav-item row">
             <a className="nav-link active col" aria-current="page" href="#">
-                    <span classNameName="mx-3 p-2">LogIn</span>
+                    <span className="mx-3 p-2">LogIn</span>
                 </a>
                 <a className="nav-link active col" aria-current="page" href="#">
-                <span classNameName="mx-3 p-2">Cart</span>
+                <span className="mx-3 p-2">Cart</span>
                 </a>
             </li>
         </ul>
