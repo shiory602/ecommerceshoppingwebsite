85b5c172ba8bed1bfdb7e1a8c676fc5723112338
unknown <gerardomontme@gmail.com>
2021-05-31 16:32:27 -0700

    -

---
+++ src/components/Products/BagModal/BagModal.jsx
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react';
+
+export default function BagModal(props) {
+
+    return (
+        <>
+        {console.log(props.data)}
+        <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="staticBackdropLabel">Product added to the Cart</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <img src={props.data[1].image} alt="photo" width='100px' />
+                    <h5>{props.data[1].title}</h5>
+                    <p>${props.data[1].price}</p>
+                    
+                    <button>Go to Cart</button>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Understood</button>
+                </div>
+                </div>
+            </div>
+        </div>  
+        </> 
+    )
+}
